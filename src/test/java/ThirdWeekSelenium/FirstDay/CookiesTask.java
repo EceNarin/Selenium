6daeb9788553928cb4f32ca8fb9c7aa7a0821f7b
EceNarin <la.crymosa321@gmail.com>
2022-05-24 17:35:28 +0300

    up

--- src/test/java/ThirdWeekSelenium/FirstDay/CookiesTask.java
+++ src/test/java/ThirdWeekSelenium/FirstDay/CookiesTask.java
@@ -1,4 +1,4 @@
-package ThirdWeekSelenium;
+package ThirdWeekSelenium.FirstDay;
 
 import org.junit.Assert;
 import org.junit.Test;
--- src/test/java/ThirdWeekSelenium/FirstDay/WebTablesTask1.java
+++ src/test/java/ThirdWeekSelenium/FirstDay/WebTablesTask1.java
@@ -1,4 +1,4 @@
-package ThirdWeekSelenium;
+package ThirdWeekSelenium.FirstDay;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
